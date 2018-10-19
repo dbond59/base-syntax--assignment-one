edb5e35f67bf31c04aaaea4388af5756ad04a436
AgentBond\dbond59 <bonddn@mail.uc.edu>
2018-10-19 18:48:19 -0400

    created two new components, added elements to those components, added multiple useroutput components, passed username via props, added state and to App component, added method to manipulate the state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
+
+  state = {
+    username: "Deyshawn"
+  }
+
+  changeNameHandler = () => {
+    // console.log('double clicked named changed');
+    // use this.setState method, it will merge what we define here with our existing state!
+    this.setState({username: "Johnathan"})
+  }
+
   render() {
-    return (
+    return(
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <UserInput/>
+      <button onDoubleClick={this.changeNameHandler}>Change Name</button>
+      <UserOutput username={this.state.username}/>
+      <UserOutput username={this.state.username}/>
+      <UserOutput username={this.state.username}/>
       </div>
     );
   }
 }
 
 export default App;
+
+// The instructions are:
+
+// ~ Create TWO new components: UserInput and UserOutput 
+// ~ UserInput should hold an input element, UserOutput two paragraphs
+// ~ Output multiple UserOutput components in the App component (any paragraph texts of your choice)
+// ~ Pass a username (of your choice) to UserOutput via props and display it there
+// ~ Add state to the App component (=> the username) and pass the username to the UserOutput component
+// ~ Add a method to manipulate the state (=> an event-handler method)
+// Pass the event-handler method reference to the UserInput component and bind it to the input-change event
+// Ensure that the new input entered by the user overwrites the old username passed to UserOutput
+// Add two-way-binding to your input (in UserInput) to also display the starting username
+// Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets
---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const useroutput = (props) => {
+    return(
+    <div>
+        <p>I'm {props.username} and I love to code. I will do whatever I can to get better!!!</p>
+        <p>Starting with this course is how I am attempting to get more practice.</p>
+    </div>
+    )
+}
+
+export default useroutput;
